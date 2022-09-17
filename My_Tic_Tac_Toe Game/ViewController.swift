f1a2dc0a15dd6967ebe58c5d8bb1eb4c8d8050d5
zannatul-fardous <zannat12.fardous@gmail.com>
2022-09-17 13:00:36 +0200

    Initial Commit

---
+++ My_Tic_Tac_Toe Game/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  My_Tic_Tac_Toe Game
+//
+//  Created by Zannatul Pinky on 2022-09-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a3: UIButton!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    @IBOutlet weak var c1: UIButton!
+    @IBOutlet weak var c2: UIButton!
+    @IBOutlet weak var c3: UIButton!
+    
+    @IBOutlet weak var turnLable: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func handleClickBtn(_ sender: Any)
+    {
+    }
+    
+   
+}
+
