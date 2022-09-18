56c3511b705aff196ba3d4176f75dd44de17a56b
zannatul-fardous <zannat12.fardous@gmail.com>
2022-09-18 12:45:35 +0200

    add handle click function

--- My_Tic_Tac_Toe Game/ViewController.swift
+++ My_Tic_Tac_Toe Game/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //Take enumeration for handleClick so we can use easily true or false
+    enum Turn {
+        case CROSS
+        case NAUGHT
+    }
+    
     @IBOutlet weak var a1: UIButton!
     @IBOutlet weak var a2: UIButton!
     @IBOutlet weak var a3: UIButton!
@@ -24,13 +30,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var turnLable: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     
+    var crossLable = "x"
+    var naughtLable = "o"
+    
+    var currentTurn = Turn.CROSS
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-    @IBAction func clickBtn(_ sender: UIButton)
-    {
+  
+    @IBAction func clickBtn(_ sender: UIButton) {
+        
+    }
+    
+    //This function for all buttons activities
+    func handleClick(_ sender: UIButton) {
+        if sender.title(for: .normal) == nil {
+            
+            if currentTurn == Turn.CROSS {
+                sender.setTitle(crossLable, for: .normal)
+                currentTurn = Turn.NAUGHT
+                turnLable.text = naughtLable
+                
+            }else if currentTurn == Turn.NAUGHT {
+                    sender.setTitle(naughtLable, for: .normal)
+                    currentTurn = Turn.CROSS
+                    turnLable.text = crossLable            }
+        }
     }
 }
 
